899bd29cb47e1be9078704f786a24168c3a37a04
Thomas Chollet <t.chollet85180@outlook.fr>
2023-01-27 09:30:12 +0100

    Partie 3: ajout d'un style sur les boutons actifs/inactifs

--- Partie3/js/index.js
+++ Partie3/js/index.js
@@ -6,6 +6,23 @@ function afficherPanel(){
     buttons.forEach((button) => {
         // On ajoute à chacun un event click
         button.addEventListener("click", (event) => {
+
+            // On va chercher le parent des boutons
+            const parentTab = button.closest('.tabs') ;
+
+            // Pour chaque <li> enfant :
+            parentTab.querySelectorAll('.tabs_item').forEach((item) => {
+
+                // On sélectionne tous les liens et on les rend inactifs
+                item.querySelectorAll('.tabs_link').forEach((e) =>{
+                    e.classList.remove("active")
+                })
+                
+            });
+            
+            // Puis on ajoute au bouton clické la classe active :
+            button.classList.add('active');
+
             // On va chercher l'ID du tab dans le href dans le DOM, et supprimer le # en trop
             const tabId = event.target.getAttribute('href').replace('#', '');
 
@@ -13,8 +30,8 @@ function afficherPanel(){
             const tab = document.getElementById(tabId);
             
             // On va chercher le parent du tab, puis sélectionner tous ses panels enfants,
-            const parent = tab.closest('.tabs_panels');
-            parent.querySelectorAll('.tabs_panel').forEach((panel) => {
+            const parentPanel = tab.closest('.tabs_panels');
+            parentPanel.querySelectorAll('.tabs_panel').forEach((panel) => {
 
                 // Puis on cache chacun des panels
                 panel.classList.remove("active");
